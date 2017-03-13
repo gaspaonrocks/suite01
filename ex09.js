90b7ee45c342a82898eea8d1eb3b8bffc8b3131c
Sacha GRASSO <gr.sacha.glgm@gmail.com>
2017-03-13 16:41:53 +0100

    résolution exo 9

--- ex09.js
+++ ex09.js
@@ -22,3 +22,26 @@ boostedEvenAddition([4, 5, 6]);
 boostedEvenAddition([1, 3, 4, 5, 4, 6, 9, 8, 11, 10, 11, 12]);
 
 //  écrire votre code sous ce commentaire
+
+
+function isEven(nb){
+    var result = nb%2;
+    if (result == 0){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+
+function boostedEvenAddition (array){
+    var evenSum = 0;
+    for (var i=0; i<array.length ; i++){
+        var checkNum = isEven(array[i]);
+        if (checkNum == 1){
+            evenSum += array[i];
+        }
+    }
+    console.log(evenSum);
+    return evenSum;
+}
